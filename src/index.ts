9189942098cb330908a61435a3cb161dba4a6942
Alexander Holovin <dvjmixxxer@gmail.com>
2018-07-02 03:44:18 +0300

    Echo bot

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,10 @@ import { to } from 'await-to-js';
 import { ParsedUrlQuery } from 'querystring';
 import { stringify } from 'circular-json';
 
+import express, { Router, Application, Response, Request } from 'express';
+import * as builder from 'botbuilder';
+import { ChatConnector, MemoryBotStorage, UniversalBot } from 'botbuilder';
+
 // configs
 const config = nconf.env().file({file: './config/dev.json'});
 const req = request.defaults(config.get('http:headers'));
@@ -22,4 +26,55 @@ moment.tz.setDefault(config.get('system.timezone'));
 prettyError.start();
 // end configs
 
-console.log('test ok!');
+// ???
+class Bot {
+    private config = config.get('system');
+
+    private app: Application;
+    private connector: ChatConnector;
+    private storage: MemoryBotStorage;
+    private bot: UniversalBot;
+
+    public constructor() {
+        this.setupBot();
+        this.setupServer();
+    }
+
+    private setupBot(): void {
+        this.connector = new ChatConnector({
+            appId:       this.config.appId,
+            appPassword: this.config.appPassword,
+            gzipData: true,
+        });
+
+        this.storage = new MemoryBotStorage();
+
+        this.bot = new UniversalBot(
+            this.connector, session => {
+                session.send(`>>> ${session.message.text}`);
+            }
+        ).set('storage', this.storage);
+    }
+
+    private setupServer() {
+        this.app = express();
+        this.setupApp([
+            express.urlencoded({ extended: true }),
+            express.json(),
+        ]);
+
+        this.app.post('/api/messages', this.connector.listen());
+
+        this.app.listen(this.config.server.port);
+    }
+
+    private setupApp(middlewares: any[]) {
+        for (const middleware of middlewares) {
+            this.app.use(middleware);
+        }
+    }
+}
+
+(async () => {
+    new Bot();
+})();
